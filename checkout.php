35516853ffb41745b1c29b1f22e5ab68a425aa43
YuraProjects <devonwullan@gmail.com>
2022-05-18 05:50:52 +0800

    Video 16: interface Checkout

--- checkout.php
+++ checkout.php
@@ -41,7 +41,84 @@ if (!isset($_SESSION['pelanggan'])) {
             </ul>
         </div>
     </nav>
-    <pre><?php print_r($_SESSION['pelanggan']) ?></pre>
+    <!-- Konten -->
+    <section class="konten">
+        <div class="container">
+            <h1>Keranjang Belanja</h1>
+            <hr>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>NO</td>
+                        <td>Produk</td>
+                        <td>Harga</td>
+                        <td>Jumlah</td>
+                        <td>SubHarga</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $totalbelanja = 0; ?>
+                    <?php $nomor = 1; ?>
+                    <?php foreach ($_SESSION['keranjang'] as $id_produk => $jumlah) : ?>
+                        <!-- menampilkan produk yg sedang diperulangkan berdasarkan id_produk -->
+                        <?php
+                        $ambil = $koneksi->query("SELECT * FROM produk WHERE id_produk='$id_produk'");
+                        $pecah = $ambil->fetch_assoc();
+                        $subharga = $pecah['harga_produk'] * $jumlah;
+                        ?>
+                        <tr>
+                            <td><?php echo $nomor ?></td>
+                            <td><?php echo $pecah['nama_produk'] ?></td>
+                            <td>Rp.<?php echo number_format($pecah['harga_produk']) ?></td>
+                            <td><?php echo $jumlah ?></td>
+                            <td>Rp.<?php echo number_format($subharga) ?></td>
+                        </tr>
+                        <?php $nomor++; ?>
+                        <?php $totalbelanja += $subharga; ?>
+                    <?php endforeach; ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th colspan="4">Total Belanja :</th>
+                        <th>Rp.<?php echo number_format($totalbelanja) ?></th>
+                    </tr>
+                </tfoot>
+
+            </table>
+
+            <form action="POST">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input type="text" readonly class="form-control" value="<?php echo $_SESSION['pelanggan']['nama_pelanggan'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input type="text" readonly class="form-control" value="<?php echo $_SESSION['pelanggan']['telepon_pelanggan'] ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <select name="id_ongkir" id="" class="form-control">
+                                <option value="">Pilih Ongkos Kirim</option>
+                                <?php
+                                $ambil = $koneksi->query("SELECT * FROM ongkir");
+                                while ($perongkir = $ambil->fetch_assoc()) {
+                                ?>
+                                    <option value="<?php echo $perongkir['id_ongkir'] ?>">
+                                        <?php echo $perongkir['nama_kota'] ?> -
+                                        Rp.<?php echo $perongkir['tarif'] ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-primary" name="checkout">Checkout</button>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
